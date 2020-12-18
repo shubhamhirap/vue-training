97b51e2cbe8ab7528da52f6e93d6d6ec1edee21c
shubham hirap <shirap07@gmail.com>
2020-12-18 18:24:01 +0530

    text binding syntax ways

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
-<template></template>
+<template>
+  <div>
+    <!-- text-binding with Mustache syntax -->
+    <div>{{ greet }} {{ name }}</div>
+    <!-- text binding with directive syntax -->
+    <div v-text="singer"></div>
+  </div>
+</template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      greet: "Hello",
+      name: "Lucifer",
+      singer: "post malone",
+    };
+  },
 };
 </script>
 
