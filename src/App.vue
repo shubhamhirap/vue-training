7368f4ae7d2b5bcefcb66fc4a1ff5436cfcda072
shubham hirap <shirap07@gmail.com>
2020-12-19 10:09:32 +0530

    classes binding ways

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,42 @@
     <!-- attr binding -->
     <h2 v-bind:id="headingId">Heading</h2>
     <button v-bind:disabled="isDisabled">Bind</button>
+
+    <!-- Binding to class -->
+    <!-- Static class -->
+    <h2 class="text-color">Static Class</h2>
+
+    <!-- v-bind class binding -->
+    <h3 v-bind:class="status">v-bind Class Binding</h3>
+
+    <!-- we can add both static and dynamic class at same time -->
+    <h4 class="text-color" v-bind:class="status">static as well as dynamic</h4>
+
+    <!-- conditionally apply class to html element -->
+    <h2 v-bind:class="isPromoted && 'promoted'">Conditional class binding</h2>
+
+    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">SoldOut? movie</h2>
+
+    <!-- class binding with array -->
+    <h3 v-bind:class="['new', 'promoted']">Newly promoted movie</h3>
+
+    <!-- class binding with array (conditional) -->
+    <h3
+      v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']"
+    >
+      Array Conditional
+    </h3>
+
+    <!-- Object conditional -->
+    <h3
+      v-bind:class="{
+        promoted: isPromoted,
+        new: !isSoldOut,
+        'sold-out': isSoldOut,
+      }"
+    >
+      Object Conditional
+    </h3>
   </div>
 </template>
 
@@ -21,6 +57,9 @@ export default {
       singer: "<strong>post malone</strong>",
       headingId: "heading",
       isDisabled: true,
+      status: "danger",
+      isPromoted: true,
+      isSoldOut: true,
     };
   },
 };
@@ -34,5 +73,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.text-color {
+  color: #908;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: green;
+}
+.sold-out {
+  color: red;
 }
 </style>
