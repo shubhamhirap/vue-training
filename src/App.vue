46f646429320748d0a0ba3ffa97106f24279184a
shubham hirap <shirap07@gmail.com>
2021-01-10 16:24:24 +0530

    computed properties and v-for

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,18 @@
     <h2>FullName - {{ firstName }} {{ lastName }}</h2>
     <!-- with computed property -->
     <h2>Fullname (with computed property) - {{ fullName }}</h2>
-    <button @click="items.push({ id: 4, title: 'audi', price: 21000 })">
+    <button @click="items.push({ id: 7, title: 'audi', price: 21000 })">
       Add Item
     </button>
     <h3>Computed Total - ${{ total }}</h3>
     <h3>Method Total - ${{ getTotal() }}</h3>
     <input type="text" v-model="country" />
+    <div v-for="item in items" :key="item.id">
+      <h2 v-if="item.price > 15000">{{ item.title }} {{ item.price }}</h2>
+    </div>
+    <h2 v-for="item in expensiveItems" :key="item.id">
+      Computed -> {{ item.title }} {{ item.price }}
+    </h2>
   </div>
 </template>
 
@@ -23,6 +29,9 @@ export default {
         { id: 1, title: "Honda", price: 12000 },
         { id: 2, title: "ford", price: 10000 },
         { id: 3, title: "BMW", price: 22000 },
+        { id: 4, title: "Hundai", price: 9000 },
+        { id: 5, title: "Dodge", price: 18000 },
+        { id: 6, title: "volkwagon", price: 14000 },
       ],
       country: "",
     };
@@ -46,6 +55,9 @@ export default {
         (total, current) => (total = total + current.price),
         0
       );
+    },
+    expensiveItems() {
+      return this.items.filter((item) => item.price > 15000);
     },
   },
 };
